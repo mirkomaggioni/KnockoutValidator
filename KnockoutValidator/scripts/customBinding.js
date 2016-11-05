9de6c94b1d0c806f4425be2371a2d8de8c1b8108
Mirko Maggioni <mirko.maggioni@gmail.com>
2016-11-05 18:02:01 +0100

    Add project files.

---
+++ KnockoutValidator/scripts/customBinding.js
@@ -0,0 +1,30 @@
+﻿
+ko.bindingHandlers.isValid = {
+  init: function (element, valueAccessor, allBindings) {
+    var errorLabel = allBindings.get("errorLabel");
+
+    $("#" + errorLabel).hide();
+  },
+  update: function (element, valueAccessor, allBindings) {
+    var errorLabel = allBindings.get("errorLabel");
+    var callBack = allBindings.get("callBack");
+    var onSubmit = allBindings.get("onSubmit");
+    var value = ko.unwrap(valueAccessor());
+    var errors = $("label.error:visible");
+
+    if (value === "" && onSubmit != undefined && onSubmit()) {
+      $("#" + errorLabel).show();
+    }
+    else {
+      $("#" + errorLabel).hide();
+
+      if (errors.length === 1 && errors.attr("id") === errorLabel) {
+        onSubmit(false);
+      }
+      else if ((callBack != null) && onSubmit() && errors.length === 0) {
+        callBack();
+      }
+    }
+    
+  }
+};
---
+++ KnockoutValidator/scripts/viewModel.js
@@ -0,0 +1,21 @@
+﻿
+function ViewModel() {
+  var self = this;
+
+  self.FirstName = ko.observable("");
+  self.LastName = ko.observable("");
+  self.OnSubmit = ko.observable(false);
+  self.OnSendData = ko.observable(false);
+
+  self.Submit = function () {
+    self.OnSubmit(true);
+  }
+
+  self.Unsubmit = function () {
+    self.OnSubmit(false);
+  }
+
+  self.SendData = function () {
+    self.OnSendData(true);
+  }
+}
